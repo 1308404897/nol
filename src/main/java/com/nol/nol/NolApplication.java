ae00327e75c6742c6df5659b4bfe85edeb6e7663
Administrator <Administrator@PC-201810232102>
2018-11-05 16:08:47 +0800

    init

---
+++ src/main/java/com/nol/nol/NolApplication.java
@@ -0,0 +1,12 @@
+package com.nol.nol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NolApplication.class, args);
+	}
+}
